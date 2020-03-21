4ce6ae80a712cc8c9fb5d480998059abec926f7f
pavelNM <papi1998@mail.bg>
2020-03-21 14:43:55 +0200

    Created master layout

---
+++ Merchain/Data/Merchain.Data/Seeding/UserRoleSeeder.cs
@@ -0,0 +1,49 @@
+﻿namespace Merchain.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Merchain.Common;
+    using Merchain.Data.Models;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    internal class UserRoleSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await SeedUserRoleAsync(dbContext, roleManager, userManager, "pavel", GlobalConstants.AdministratorRoleName);
+        }
+
+        private static async Task SeedUserRoleAsync(
+            ApplicationDbContext dbContext,
+            RoleManager<ApplicationRole> roleManager,
+            UserManager<ApplicationUser> userManager,
+            string username,
+            string roleName)
+        {
+            var user = await userManager.FindByNameAsync(username);
+            var role = await roleManager.FindByNameAsync(roleName);
+
+            bool exists = dbContext.UserRoles.Any(x => x.UserId == user.Id && x.RoleId == role.Id);
+
+            if (exists)
+            {
+                return;
+            }
+
+            await dbContext.UserRoles.AddAsync(new IdentityUserRole<string>()
+            {
+                UserId = user.Id,
+                RoleId = role.Id,
+            });
+
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Merchain/Data/Merchain.Data/Seeding/UsersSeeder.cs
@@ -0,0 +1,42 @@
+﻿namespace Merchain.Data.Seeding
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Merchain.Data.Models;
+
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class UsersSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            await SeedUsersAsync(userManager);
+        }
+
+        private static async Task SeedUsersAsync(UserManager<ApplicationUser> userManager)
+        {
+            var user = await userManager.FindByNameAsync("pavel");
+            if (user == null)
+            {
+                var result = await userManager.CreateAsync(
+                    new ApplicationUser()
+                    {
+                        UserName = "pavel",
+                        Email = "pavel.nm@abv.bg",
+                        EmailConfirmed = true,
+                    },
+                    password: "pavel98");
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception(string.Join(Environment.NewLine, result.Errors.Select(e => e.Description)));
+                }
+            }
+        }
+    }
+}
